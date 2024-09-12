7d51407b1dfef380eb963ae4975873d5852a7644
DavidValencia526 <davidvalencia0526@gmail.com>
2024-09-12 02:56:08 -0500

    creados bien los procedimientos y eventos para que se eliminen registros y usuarios no confirmados

--- procedures.sql
+++ procedures.sql
@@ -1,3 +1,5 @@
+-- Procedimiento limpiar registros de confirmacion de usuario y verificacion en 2 pasos
+
 DELIMITER //
 
 CREATE PROCEDURE cleanup_expired_records()
@@ -13,7 +15,39 @@ END //
 
 DELIMITER ;
 
--- Crear un evento que ejecute el procedimiento cada hora
+-- Eliminar evento si existe
+DROP EVENT IF EXISTS cleanup_expired_records_event;
+
+-- Crear un evento que ejecute el procedimiento cada 10 minutos
 CREATE EVENT cleanup_expired_records_event
-ON SCHEDULE EVERY 1 HOUR
-DO CALL cleanup_expired_records();
+ON SCHEDULE EVERY 10 MINUTE
+DO CALL cleanup_expired_records()
+
+
+-- Procedimiento y evento de eliminación de usuarios no confirmados
+
+DELIMITER //
+
+CREATE PROCEDURE cleanup_pending_users()
+BEGIN
+    -- Eliminar los registros de usuario_rol asociados con usuarios que tienen state_id = el ID de estado 'pending'
+    DELETE FROM usuario_rol
+    WHERE usuario_id IN (
+        SELECT id FROM usuario 
+        WHERE state_id = (SELECT id FROM estado_usuario WHERE nombre = 'pending')
+    );
+
+    -- Eliminar los usuarios con state_id = el ID de estado 'pending'
+    DELETE FROM usuario
+    WHERE state_id = (SELECT id FROM estado_usuario WHERE nombre = 'pending');
+END //
+
+DELIMITER ;
+
+-- Eliminar evento si existe
+DROP EVENT IF EXISTS cleanup_pending_users_event;
+
+-- Crear un evento que ejecute el procedimiento cada 10 minutos
+CREATE EVENT cleanup_pending_users_event
+ON SCHEDULE EVERY 10 MINUTE
+DO CALL cleanup_pending_users();
