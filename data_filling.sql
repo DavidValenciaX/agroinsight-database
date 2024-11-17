11bc0e17e8cee650c4d2c2eb28f0fce6477e5243
DavidValencia526 <davidvalencia0526@gmail.com>
2024-11-17 16:33:36 -0500

    agregadas tablas y registros para los logs

--- data_filling.sql
+++ data_filling.sql
@@ -47,11 +47,13 @@ INSERT INTO unidad_medida (nombre, abreviatura, categoria_id) VALUES
 ('Mililitro', 'mL', 3),
 ('Metro cúbico', 'm³', 3),
 ('Galón', 'gal', 3),
+('Bolsa 60000 semillas', 'bolsa 60k sem', 3),
 -- Masa
 ('Kilogramo', 'kg', 4),
 ('Gramo', 'g', 4),
 ('Tonelada', 't', 4),
 ('Libra', 'lb', 4),
+('Bulto 50 kilogramos', 'bulto 50kg', 4),
 -- Tiempo
 ('Segundo', 's', 5),
 ('Minuto', 'min', 5),
@@ -74,22 +76,18 @@ INSERT INTO unidad_medida (nombre, abreviatura, categoria_id) VALUES
 -- Rendimiento
 ('Toneladas por hectárea', 't/ha', 9),
 ('Kilogramos por hectárea', 'kg/ha', 9),
--- Presión (asumiendo que es categoria_id = 10)
+-- Presión 
 ('Hectopascal', 'hPa', 10),
 ('Milibar', 'mbar', 10),
-
--- Porcentaje (asumiendo que es categoria_id = 11)
+-- Porcentaje
 ('Porcentaje', '%', 11),
-
--- Velocidad (asumiendo que es categoria_id = 12)
+-- Velocidad
 ('Metro por segundo', 'm/s', 12),
 ('Kilómetro por hora', 'km/h', 12),
 ('Milla por hora', 'mph', 12),
-
--- Ángulo (asumiendo que es categoria_id = 13)
+-- Ángulo
 ('Grado', '°', 13),
-
--- Tasa de precipitación (asumiendo que es categoria_id = 14)
+-- Tasa de precipitación
 ('Milímetro por hora', 'mm/h', 14);
 
 
@@ -365,3 +363,433 @@ INSERT INTO tipo_suelo (nombre, descripcion, color_id, textura_id, munsell_id, c
  7, 7, 7,
  'Fertilidad moderada, buen drenaje, estructura moderadamente estable',
  'Manejo de acidez, incorporación de materia orgánica, rotación de cultivos');
+
+-- Insertar insumos agricolas
+
+--- insumos agricolas
+
+ -- Insertar categorías de insumos agrícolas
+INSERT INTO categoria_insumo_agricola (nombre, descripcion) VALUES
+('Fertilizantes', 'Insumos para nutrición y fertilización del cultivo de maíz'),
+('Fungicidas', 'Insumos para control de enfermedades fúngicas'),
+('Insecticidas', 'Insumos para control de plagas e insectos'),
+('Herbicidas', 'Insumos para control de malezas'),
+('Semillas', 'Material genético certificado para siembra'),
+('Acondicionadores de Suelo', 'Insumos para mejorar las condiciones del suelo'),
+('Otros Insumos', 'Insumos complementarios para el manejo del cultivo');
+
+-- Insertar insumos agrícolas
+INSERT INTO insumo_agricola (categoria_id, nombre, descripcion, unidad_medida_id, costo_unitario, stock_actual) VALUES
+-- Fertilizantes
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fertilizantes'),
+ 'Urea 46-0-0',
+ 'Fertilizante nitrogenado granular para desarrollo vegetativo',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bulto 50kg'),
+ 180000,
+ 125),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fertilizantes'),
+ 'DAP 18-46-0',
+ 'Fosfato Diamónico para desarrollo radicular',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bulto 50kg'),
+ 195000,
+ 125),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fertilizantes'),
+ 'KCl 0-0-60',
+ 'Cloruro de Potasio para llenado de grano',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bulto 50kg'),
+ 165000,
+ 125),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fertilizantes'),
+ 'Nutrimon 15-15-15',
+ 'Fertilizante compuesto NPK balanceado para etapas iniciales',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bulto 50kg'),
+ 160000,
+ 125),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fertilizantes'),
+ 'Abocol Triple 15',
+ 'Fertilizante granular balanceado para desarrollo general',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bulto 50kg'),
+ 155000,
+ 125),
+
+-- Fungicidas
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fungicidas'),
+ 'Mole SC325',
+ 'Fungicida sistémico para control de enfermedades foliares en maíz',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 180000,
+ 45),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fungicidas'),
+ 'Nativo SC300',
+ 'Fungicida de amplio espectro para control de roya y tizón',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 195000,
+ 45),
+
+-- Silvacur Combi EC300
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fungicidas'),
+ 'Silvacur Combi EC300',
+ 'Fungicida preventivo y curativo para enfermedades foliares',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 170000,
+ 45),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fungicidas'),
+ 'Mancozeb 80 WP',
+ 'Controla hongos como roya, mildiu y mancha de hoja',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'kg'),
+ 27500,
+ 45),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fungicidas'),
+ 'Carbendazim CRL 500 SC',
+ 'Fungicida sistémico preventivo y curativo',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 55000,
+ 45),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Fungicidas'),
+ 'Amistar Top',
+ 'Fungicida sistémico de Syngenta para control de complejo mancha de asfalto',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 280000,
+ 45),
+
+-- Insecticidas
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Audax FS600',
+ 'Insecticida para tratamiento de semillas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 280000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Belt SC480',
+ 'Control de lepidópteros en cultivos de maíz',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 320000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Connect Duo OD300',
+ 'Insecticida de amplio espectro para control de plagas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 185000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Destello SC',
+ 'Control de gusano cogollero y barrenadores',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 145000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Dinastia EC100',
+ 'Insecticida piretroide para control de plagas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 98000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Dipel WG6,4',
+ 'Insecticida biológico para control de orugas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'kg'),
+ 125000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Lorsban 4EC',
+ 'Insecticida organofosforado de amplio espectro',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 65000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Lambda-cihalotrina',
+ 'Controla gusanos e insectos chupadores',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 25000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Engeo',
+ 'Insecticida de Syngenta para control de cogollero y chinches',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 195000,
+ 35),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Insecticidas'),
+ 'Fiprogent',
+ 'Insecticida de contacto para control de cogollero y gusanos',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 145000,
+ 35),
+
+-- Herbicidas
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Herbicidas'),
+ 'Roundup Activo',
+ 'Herbicida sistémico de amplio espectro',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 45000,
+ 85),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Herbicidas'),
+ 'Roundup Brio',
+ 'Herbicida post-emergente para control de malezas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 42000,
+ 85),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Herbicidas'),
+ 'Atrazina Vecol 90 WG',
+ 'Herbicida selectivo pre y post emergente',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'kg'),
+ 45000,
+ 85),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Herbicidas'),
+ 'Glifosato Roundup 480 SL',
+ 'Herbicida sistémico no selectivo',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 38000,
+ 85),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Herbicidas'),
+ 'Pendimetalina Vecol 400 EC',
+ 'Herbicida pre-emergente, evita el crecimiento de malezas desde la semilla',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 32500,
+ 85),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Herbicidas'),
+ 'Amina 720 SL',
+ 'Herbicida hormonal selectivo para control de malezas de hoja ancha',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 35000,
+ 85),
+
+-- Semillas
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'AGROSAVIA V-117',
+ 'Maíz amarillo de doble propósito (grano y silo), rústico con buen porte y sistema radicular. Adaptado a la región Caribe húmeda',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 750000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'AGROSAVIA V-160 QPM',
+ 'Variedad de maíz blanco con alto contenido de lisina. Recomendado para Valles del Sinú y sabanas de Córdoba, Sucre y Bolívar',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 780000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'AGROSAVIA V-121',
+ 'Maíz amarillo de doble propósito adaptado a la región Caribe húmeda, especialmente Valle del Sinú y sabanas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 750000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'AGROSAVIA V-116 HAWAII DULCE',
+ 'Maíz dulce amarillo, susceptible al gusano cogollero y Cercospora zeae-maydis pero mínimamente afectado',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 820000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'CORPOICA V-114',
+ 'Variedad de maíz amarillo',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 720000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'CORPOICA V-115 Dulce',
+ 'Variedad de maíz dulce amarillo, susceptible al gusano cogollero y Cercospora zeae-maydis',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 800000,
+ 12),
+
+-- Continuación Semillas
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'CORPOICA V-159',
+ 'Variedad de maíz blanco',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 720000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'HR-ORO',
+ 'Híbrido de maíz amarillo apto para grano y silo, excelente forraje con alta calidad nutricional',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 850000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'ICA V-305',
+ 'Variedad de maíz amarillo duro de porte medio, uniforme, con buen vigor, estabilidad y alto potencial de rendimiento',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 720000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'ICA V-109',
+ 'Maíz amarillo para grano o silo',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 700000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'ICA-V 156',
+ 'Variedad de maíz blanco para grano o silo',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 700000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'SGBIOH2',
+ 'Híbrido de maíz biofortificado con alto contenido de zinc, desarrollado por CIMMYT para reducir la malnutrición en Colombia',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 880000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'Pioneer P3966',
+ 'Híbrido de maíz amarillo de alto rendimiento para zona cafetera y valles interandinos',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 920000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'Dekalb DK7088',
+ 'Híbrido de maíz amarillo tecnología VT Triple Pro para control de plagas',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 980000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'DK 7088',
+ 'Híbrido amarillo de alto rendimiento',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 850000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'P30F35',
+ 'Híbrido blanco tolerante a sequía',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bolsa 60k sem'),
+ 820000,
+ 12),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Semillas'),
+ 'FNC 318',
+ 'Variedad mejorada de polinización abierta',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'kg'),
+ 25000,
+ 12),
+
+-- Acondicionadores de Suelo
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Acondicionadores de Suelo'),
+ 'Cal Agrícola Dolomita BM',
+ 'Ajusta el pH del suelo, mejora absorción de nutrientes',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'bulto 50kg'),
+ 20000,
+ 200),
+
+-- Otros Insumos
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Otros Insumos'),
+ 'Cosmo-Aguas',
+ 'Coadyuvante para aplicación de agroquímicos',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 28000,
+ 65),
+
+((SELECT id FROM categoria_insumo_agricola WHERE nombre = 'Otros Insumos'),
+ 'Nutrifoliar Completo',
+ 'Fertilizante foliar multinutrientes',
+ (SELECT id FROM unidad_medida WHERE abreviatura = 'L'),
+ 45000,
+ 65);
+
+-- Insertar datos de maquinaria
+
+INSERT INTO tipo_maquinaria_agricola (nombre, descripcion) VALUES
+('Tractor', 'Vehículo principal para diversas labores agrícolas'),
+('Sembradora', 'Maquinaria especializada para la siembra de precisión'),
+('Fumigadora', 'Equipo para aplicación de agroquímicos y fertilizantes'),
+('Cosechadora', 'Maquinaria para la recolección del cultivo'),
+('Arado', 'Implemento para preparación inicial del suelo'),
+('Rastra', 'Implemento para preparación secundaria del suelo'),
+('Cultivadora', 'Implemento para control de malezas entre hileras'),
+('Fertilizadora', 'Equipo para aplicación de fertilizantes sólidos'),
+('Desgranadora', 'Máquina para separar los granos de la mazorca'),
+('Remolque', 'Equipo para transporte de insumos y cosecha');
+
+INSERT INTO maquinaria_agricola (tipo_maquinaria_id, nombre, descripcion, modelo, numero_serie, costo_hora) VALUES
+-- Tractores
+(1, 'Tractor John Deere 6110J', 'Tractor mediano para labores generales', '6110J', 'JD6110-2023-001', 180000.00),
+(1, 'Tractor Massey Ferguson 4275', 'Tractor versátil para múltiples tareas', 'MF4275', 'MF4275-2022-102', 160000.00),
+(1, 'Tractor New Holland T6.130', 'Tractor potente para labores pesadas', 'T6.130', 'NH6130-2023-034', 200000.00),
+
+-- Sembradoras
+(2, 'Sembradora John Deere 1725', 'Sembradora de precisión 8 surcos', '1725', 'JD1725-2023-056', 140000.00),
+(2, 'Sembradora Massey Ferguson 555', 'Sembradora mecánica 6 surcos', 'MF555', 'MF555-2022-089', 120000.00),
+
+-- Fumigadoras
+(3, 'Fumigadora Jacto Advance 3000', 'Fumigadora de arrastre 3000L', 'Advance 3000', 'JA3000-2023-045', 100000.00),
+(3, 'Fumigadora Berthoud Tracker 3200', 'Fumigadora autopropulsada', 'Tracker 3200', 'BT3200-2023-012', 160000.00),
+
+-- Cosechadoras
+(4, 'Cosechadora John Deere S780', 'Cosechadora de alta capacidad', 'S780', 'JDS780-2023-007', 480000.00),
+(4, 'Cosechadora Case IH 7240', 'Cosechadora axial-flow', '7240', 'CI7240-2022-023', 440000.00),
+
+-- Arados
+(5, 'Arado de Discos Baldan', 'Arado de 4 discos para preparación profunda', 'NVCR-4', 'BD2023-078', 60000.00),
+(5, 'Arado de Vertedera Kverneland', 'Arado reversible de 3 surcos', 'ES85', 'KV2023-156', 72000.00),
+
+-- Rastras
+(6, 'Rastra de Discos Tatu', 'Rastra de 28 discos', 'GAPCR', 'RT2023-234', 80000.00),
+(6, 'Rastra de Dientes Baldan', 'Rastra niveladora', 'CRNL', 'RB2023-567', 60000.00),
+
+-- Cultivadoras
+(7, 'Cultivadora John Deere 2210', 'Cultivadora de campo 30 pies', '2210', 'JD2210-2023-089', 100000.00),
+(7, 'Cultivadora Case IH Tiger-Mate', 'Cultivadora de precisión', '255', 'CI255-2023-123', 112000.00),
+
+-- Fertilizadoras
+(8, 'Fertilizadora Amazone ZA-M', 'Fertilizadora de doble disco', 'ZA-M 1501', 'AM2023-345', 88000.00),
+(8, 'Fertilizadora Kuhn Axis', 'Fertilizadora de precisión', 'AXIS 40.2', 'KH2023-678', 100000.00),
+
+-- Desgranadoras
+(9, 'Desgranadora Vencedora', 'Desgranadora estacionaria', 'D-1000', 'VD2023-890', 72000.00),
+(9, 'Desgranadora Nogueira', 'Desgranadora con motor eléctrico', 'DM-50', 'ND2023-234', 60000.00),
+
+-- Remolques
+(10, 'Remolque JF', 'Remolque de volteo 12 toneladas', 'CarretaJF12000', 'JF2023-456', 48000.00),
+(10, 'Remolque Metalfor', 'Remolque granelero 15 toneladas', 'Gran15000', 'MF2023-789', 60000.00);
+
+-- Inserts para logs
+
+-- Inserción de tipos de acciones
+INSERT INTO tipo_accion_log (nombre, descripcion) VALUES
+('CREAR', 'Acción de crear un nuevo registro en el sistema'),
+('ACTUALIZAR', 'Acción de modificar un registro existente'),
+('ELIMINAR', 'Acción de eliminar un registro del sistema'),
+('INICIAR_SESION', 'Acción de inicio de sesión en el sistema'),
+('CERRAR_SESION', 'Acción de cierre de sesión en el sistema'),
+('VISUALIZAR', 'Acción de consultar o ver información'),
+('EXPORTAR', 'Acción de exportar datos del sistema'),
+('IMPORTAR', 'Acción de importar datos al sistema'),
+('GENERAR_REPORTE', 'Acción de generar un reporte'),
+('REGISTRAR_COSTOS', 'Acción de registrar costos de una tarea'),
+('REGISTRAR_COSECHA', 'Acción de registrar datos de cosecha'),
+('REGISTRAR_ANALISIS_SUELO', 'Acción de registrar un análisis de suelo'),
+('REGISTRAR_CLIMA', 'Acción de registrar datos climáticos'),
+('ASIGNAR_ROL', 'Acción de asignar un rol a un usuario'),
+('CAMBIAR_ESTADO', 'Acción de cambiar el estado de un registro');
