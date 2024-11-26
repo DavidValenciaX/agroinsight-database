3d385bbdbaceae97728b5fa9748fdd7e30296330
DavidValencia526 <davidvalencia0526@gmail.com>
2024-11-25 20:31:50 -0500

    trigger mejorado para que se cree solo en las tablas que no tienen trigger

--- script_triggers_postgresql.sql
+++ script_triggers_postgresql.sql
@@ -13,16 +13,24 @@ DECLARE
     rec RECORD;
 BEGIN
     FOR rec IN
-        SELECT table_name
-        FROM information_schema.columns
-        WHERE column_name = 'fecha_modificacion'
-        AND table_schema = 'public'
+        SELECT DISTINCT t.table_name
+        FROM information_schema.columns t
+        WHERE t.column_name = 'fecha_modificacion'
+        AND t.table_schema = 'public'
+        AND NOT EXISTS (
+            SELECT 1
+            FROM information_schema.triggers tr
+            WHERE tr.trigger_name = 'actualizar_fecha_modificacion_' || t.table_name
+            AND tr.event_object_schema = t.table_schema
+            AND tr.event_object_table = t.table_name
+        )
     LOOP
         EXECUTE format('
             CREATE TRIGGER actualizar_fecha_modificacion_%I
             BEFORE UPDATE ON %I
             FOR EACH ROW
             EXECUTE FUNCTION actualizar_fecha_modificacion();', rec.table_name, rec.table_name);
+        RAISE NOTICE 'Trigger creado para la tabla: %', rec.table_name;
     END LOOP;
 END;
 $$;
